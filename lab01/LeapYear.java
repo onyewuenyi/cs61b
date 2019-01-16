654c12d7485767dff83f06549a64f760d0ed610c
onyewuenyi <onyewuenyi@gmail.com>
2019-01-15 20:44:45 -0600

    lab01: Completed LeapYear.java

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -1,8 +1,31 @@
 /**
  * Class that determines whether or not a year is a leap year.
- * @author YOUR NAME HERE
+ * @author Charles A. Onyewuenyi
+ */
+
+
+ /**
+ * Return True if year is a LeapYear
+ * @source CS 61BL Lab 1
  */
 public class LeapYear {
+    public static boolean isLeapYear(int year) {
+      // spec:
+      // A leap year is either:
+
+      // divisible by 400 or
+      boolean divisible400 = (year % 400) == 0;
+
+      // divisible by 4
+      boolean divisible4 = (year % 4) == 0;
+      //  not divisible by 100
+      boolean notDivisible100 = (year % 100) != 0;
+
+      // divisible by 4 and not by 100.
+      boolean divis4AndNotDivis100 = divisible4 && notDivisible100;
+
+      return (divisible400 || divis4AndNotDivis100);
+    }
 
     /** Calls isLeapYear to print correct statement. */
     private static void checkLeapYear(int year) {
