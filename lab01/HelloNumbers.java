6da8a4200fca7108cddb3ded7f73168bc2382006
onyewuenyi <onyewuenyi@gmail.com>
2019-01-15 19:58:39 -0600

    Completed first part of lab01

---
+++ lab01/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int total = 0;
+        while (x < 10) {
+            total += x;
+            System.out.println(total);
+            x++;
+        }
+    }
+}
